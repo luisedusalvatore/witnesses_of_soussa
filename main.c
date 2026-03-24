8218a37595381f289ae267e218cafe234bc31af3
CarlosMonteiroTweaks <carlosmonteironeto2006@gmail.com>
2026-03-24 20:39:22 -0300

    mano, tinha uma aspas simples q tava desgraçando o código

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-# include <stdio.h>
-# include "pilha.h'
-# include <stdlib.h>
-# include <string.h>
+#include <stdio.h>
+#include "pilha.h"
+#include <stdlib.h>
+#include <string.h>
 #include <locale.h>
 
 typedef struct{
