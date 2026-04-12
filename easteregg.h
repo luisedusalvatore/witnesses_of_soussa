cb7c9a43c380d41c0e7d06c04cebda9171529905
Luis Salvatore <luiseduardosalvatore@gmail.com>
2026-04-12 10:03:28 -0300

    tranferencia dos easter eggs da main para a sua biblioteca propria

---
+++ easteregg.h
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <string.h>
+#ifndef easteregg_h
+#define easteregg_h
+void sousaEasterEgg(char *nomeAux){
+
+    if (strcmp(nomeAux, "Sousa") == 0){    // string "i" compare para facilitar possiveis variantes de um mesmo resultado (isensitive)
+        printf("\n\033[33m[SEGREDO DESBLOQUEADO] O dinossauro da computação entrou na partida!\033[0m\n");
+
+        printf("\033[33m");
+
+        printf("                      }}}}}}}}}}}}}}}{  \n");
+        printf("                   Y}}}1({}}}}}}}}}}}}}{\n");
+        printf("                   Y}}}}{{  }}}}}}}}}}}}}{\n");
+        printf("                  Y}}}}}}}  }}}}}}}}}}}}{\n");
+        printf("                   Y}}}}}}}}}}}}}}}}}}}{\n");
+        printf("                   u}}}}}}}}}}{{{{{{{{{1\n");
+        printf("                   u}}}}}}}}}{|||||/    \n");
+        printf(")}_               _}(}}}}}}}}{{{{{{{)    \n");
+        printf("1}|             |1)}}}}}}}}}}            <----- SOUSA KKKKKKKKKKKKKK\n");
+        printf("{}){{        ___{}}}}}}}}}}}}]]]|        \n");
+        printf("{}}}1        {}}}}}}}}}}}}}}}}}}}        \n");
+        printf("{}}}}}1    1}}}}}}}}}}}}}}}} )}1        \n");
+        printf("{}}}}}}}}}}}}}}}}}}}}}}}}}}}            \n");
+        printf("1{}}}}}}}}}}}}}}}}}}}}}}}}}}            \n");
+        printf("  1{}}}}}}}}}}}}}}}}}}}}}})1            \n");
+        printf("   _{{{{}}}}}}}}}}}}}}}}{{              \n");
+        printf("         {}}}}}}}}}}}}{{{                \n");
+        printf("         {}}}}}}}}}}}}1                  \n");
+        printf("          u[}}}}}{x}}}{                  \n");
+        printf("          ]}}{})     {1                  \n");
+        printf("          ]}}        {1                  \n");
+        printf("          j[}}}{     {}}}{                \n");
+
+        // reseta a cor de volta ao normal
+        printf("\033[0m\n");
+    }
+
+    if (strcmp(nomeAux, "Sanval") == 0){    // string "i" compare para facilitar possiveis variantes de um mesmo resultado (isensitive)
+        printf("\n\033[33m[SEGREDO DESBLOQUEADO] O cara do Breaking Bad? Ele também tá aqui!\033[0m\n");
+
+        printf("\033[33m");
+
+        printf("                                                                 \n");
+        printf("                                                                     \n");
+        printf("                                                                     \n");
+        printf("                                Tffjrxxnnnnnnxjf                 \n");
+        printf("                            FrrrjFjxxvcvvunxxnnnunnj             \n");
+        printf("                          rxnrjjrxxnxuuvuuunnnxnnnuuuvun         \n");
+        printf("                        jjxjjxuuuvuuvczczcvvvunrrnnunnnuxr       \n");
+        printf("                      TjFjrvXYYUYYXXYUYYYXXzXcczzXYXzcurrxr      \n");
+        printf("                     xFfFxXYUJJJJJLLLLQQmQLCCUCJUYYzzcccnjrr     \n");
+        printf("                    TffFuXXUUYUUJJLQLQQQmQQQLCCCJUYXXXYXXurjr    \n");
+        printf("                    ttFxvzYUUYUUJCLLQQQQmQQQLLLCJUYYXXzXXcxjff   \n");
+        printf("                  TTfFnuzYYUUYJCCQQmwmmqwmmQLQLCJYXzzzzzcnrfft   \n");
+        printf("                  !ttFxuzYYYUUJLLmqqppqqwqqmQQQCUYXXzvcvvxrfTt   \n");
+        printf("                  l!!lfjuzYYYUUCLQwppqqqqwwwwmQmLJYzcvvvvvunTt!  \n");
+        printf("                  !!!tFjucXXUJJJLQwpwqwqppwqqmQmQJUcvnnnczzvft!  \n");
+        printf("                  ;I!fjxvzXXYJUJCQQmmmwwwqmmwmQmQCYzczvcUcczjt!  \n");
+        printf("                  tIIfrucuncUULLQwwQmmmmmmQmQLJCCYYXXcuuXvvcrl!  \n");
+        printf("                   IlfxnnnnvcYUCLQmQQmQmwwwmmQQCUXczcunzYvvvrt!  \n");
+        printf("                   IIfjnvczYUJCJUYCwqmQmmmwmQLmQJUCXzcczzXcnrtt  \n");
+        printf("               uYJUl!FrxrxnuvYJmQmQQQJCLQYzJLLLUXvnxjFfTFrvcnTTzXv\n");
+        printf("               T:FJjTFFTtt!!tT!fFTtFTFrcUJYjtTtlIlIIIIIlltrccFnvnj\n");
+        printf("               xTTfFtjjT!i!FjFfjjt;,:ttuYXvft.,ITrxxrjTltfrxxIuxzx\n");
+        printf("               fzvrTrFffItl;:!I:lnItiltTfFTtilticl;IrI;tt!rFcjjTun\n");
+        printf("                nFl!xnjjjFFjnnzYzvYUuFjXJUzjTxYYXXXcFrrTFFFruj!fjz\n");
+        printf("                xvxrFrjvvcvjrucvzuzuxTuYCUccFxvvvXzurFjvunxrfutnnn\n");
+        printf("                 znjtfTjcXXzzuvzcccvFruzCUzznjnvuvnnxuvccuTXXcrcz \n");
+        printf("                 rnfIF!rruXYYUJJXcuFjxncCJcccFFxnvXXcccvjFfXXuvxn \n");
+        printf("                  jFlfFt!FnvvvvuuvvnFrcXLLXcufrnxxrxnnnjtFnXzrvX  \n");
+        printf("                  njTfFfjrxvvzYUCUYnvnvCQQCuvvxuczzvuunxrjxzcncY  \n");
+        printf("                  !fftTffFrnuvzUJcffttfnXYcjFrjFuccvunxrxrnzxFr   \n");
+        printf("                    FfTTFFrnucvrruF:,,:;::..,fnjjnxxrxrxruvv      \n");
+        printf("                    jFFFfFFrxxrnxjFflIi,,:,;I!frxjjTjrjjxcnn      \n");
+        printf("                     fFFFFfFjjjt!lllilI;;!IiiiIltTFxrxxjnur       \n");
+        printf("                     fFfFjfFjflI;;ii;IlIiIliI;::;I!frxrjuxx       \n");
+        printf("                     tTTfrfjti:,,::ii;;::;::,:::,,:IFnrrvjf       \n");
+        printf("                      ;ltfjTl::!fFfFjrFTtttT!!fff;;!FrjnrF        \n");
+        printf("                       .;!T!l;;ljrrFTI;:;iilTfrjTIItFfrTl         \n");
+        printf("                       .,;!tI:ilfrxF!i;:;;;iljFtIII!TfI::         \n");
+        printf("                 UzxTIi,,:ii::i;ltttI!llIIlIIiIi;;lI,,:IvJLCU     \n");
+        printf("                 zYJYvTIi,,,,,::::illIIlIIi;;;;:;:,:,,,:linnzzvX  \n");
+        printf("                vzXYJJcri:,:,,,,,,:;:;;;;:::::::,,,,,,,lt.!YwwXcUC \n");
+        printf("              YYYYUUUUYcv;i::,,,.,,:::::::,,,.,,,.,,.,ltj!nLUcuuvc \n");
+        printf("            zUYXzcUUJCUUzFlli;:,,,..,,,.,.,........,:ltfjFUUQYcccvU\n");
+        printf("          uzzXYXzYLUcXXJJYt!T!I;:,,....     .....,:i!TTFfXUJUYzcnxx\n");
+        printf("        uuzUUUUXJJUUJCLQXY!tTtli:,,.. .    ..,,,itTTFjfXzYYYntnczYXXYYYc \n");
+        printf("    XXYUUUUYXvjruzYJJUXYJJUYtTTtli;::,......,,:;lTFjrjFXXQYnUuzXYXYYYXYYXYYYY  \n");
+        printf("UUJUUJUUJJJUUUUctlfvUJLmmLJLnfrfI;;:::;:::::ilfFjxxrrYcLxvCXzYYJYYUYYYYXzXYUUJUYU\n");
+        printf("JUJJUUUUYXUJJCCJJUvf;tzmbmUUJXfxFftI;;iIIIl!TfjxuxrnXvQnXmCcJJULJUJUYYXXYUUUUUUJU\n");
+        printf("CJCLJJJJJzzYUCLLmmCYJF:;tucJmQYxnxrFT!ll!tTfFjruvrnccdcJqdvXLLJmCUJUYXczJCJJJJJCJ\n");
+        printf("LCLmLLCCJUzzXULLQQCXJUcvFilxYqLCLnjrjfTtttfjxxnuncXcwzUwbLvCCLYpmJJUYzvzJCCCJUQmL\n");
+        printf("UULwQLQLLCYzcXUCQQLYXCJYzvut;tYwwCffFFjjrrrrrxxnJLvpJzQbwnCCYLXqqCUYXvcYJCCCCQwwQ\n");
+        printf("zzQpwmQLLLCYccYQwwmLXYmmLJvXXf;rqmQdkbpwLJYJQdbwwcQqYLpQvXLYJJYddmUXccXYJCLQmqmLJ\n");
+        printf("XvmdwQwmQLLUcvYmdpqqJcCpdwmCvUzTcbbqkhaaooMaQdbwvXdUmbUcYLCJQYUbdwYccXXYCLQmwmCzU\n");
+        printf("JvQbqmqwQLLJzvYmdddpmzXqmmCCmXvcFzmQwpbhhhahwhqJuqQCdUzJLJQmwXmbpCzzXYXYCLmwmCYYQ\n");
+        printf("LvCdqwpqQLLCXzYQpbbddJcQbLQLYQwzurYJQbhahaMaQbwvCwXpmJmCJQqpQYdbQXYJUYYJLmwmCYXJQ\n");
+        printf("QcJqqqpqQQQQUYUQpkkbbmzUpbCmwYUqpcumzwhahhoawdcXkXwbmqLUQqqpJQkmJJQQUYJLQwmCYXYJm\n");
+
+        printf("\033[0m\n");
+    }
+
+}
+#endif
