4a8a23da54b8c150dfff9193f3a999c48c5bb325
Luis Salvatore <luiseduardosalvatore@gmail.com>
2026-05-10 14:05:35 -0300

    criação do tp_casa para ser usado nas casas do tabuleiro

--- tipos.h
+++ tipos.h
@@ -1,10 +1,11 @@
+#include "listade.h";
 # ifndef tipos_h
 # define tipos_h
 typedef struct{		// struct de jogadores
 
     char nome[25];
     int cor;
-    int posicao;
+    tp_no *posicao;
 
 }Player;
 
@@ -13,5 +14,11 @@ typedef struct{
     char enunciado[300];
     char resposta;
 } tp_pergunta;
+
+typedef struct {
+    int cor[4];
+    int tira_carta;
+    int posicao;
+} tp_casa;
 	
 # endif
