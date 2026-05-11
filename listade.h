cf88b046046d5a9e85ba6afd117f39e89596de66
dudivis-ide <eduardocostabloisi2007@gmail.com>
2026-05-11 09:27:09 -0300

    listade.h adaptado para dinamico

--- listade.h
+++ listade.h
@@ -1,9 +1,8 @@
+#ifndef listade_h
+#define listade_h
 #include <stdio.h>
 #include <stdlib.h>
 #include "tipos.h"
-#ifndef listade_h
-#define listade_h
-
 
 tp_listade *inicializa_listade(){
     tp_listade *lista=(tp_listade*) malloc(sizeof(tp_listade));
@@ -11,24 +10,30 @@ tp_listade *inicializa_listade(){
     lista->fim = NULL;
     return lista;
 }
+
 int listade_vazia(tp_listade *lista){
     if((lista->ini == NULL) && (lista->fim == NULL)) return 1;
     return 0;
 }
-tp_no *aloca(){
+
+tp_no *aloca_listade(){
     tp_no *pt;
     pt=(tp_no*) malloc(sizeof(tp_no));
+    return pt;
 }
+
 int insere_listade_no_fim(tp_listade *lista, int posicao, int carta){
     tp_no *novo;
-    novo = aloca();
+    novo = aloca_listade();
     if(!novo) return 0;
+
     novo->info.cor[0] = 0;
     novo->info.cor[1] = 0;
     novo->info.cor[2] = 0;
     novo->info.cor[3] = 0;
     novo->info.posicao = posicao;
     novo->info.tira_carta = carta;
+
     if(listade_vazia(lista)){
         novo->prox = NULL;
         novo->ant = NULL;
@@ -42,34 +47,41 @@ int insere_listade_no_fim(tp_listade *lista, int posicao, int carta){
     }
     return 1;
 }
+
 void imprime_listade(tp_listade *lista, int ordem){
     if(lista==NULL)
-    printf("Lista nao inicializada");
+        printf("Lista nao inicializada");
     else{
         tp_no *atu;
         switch(ordem){
-            case 1: atu = lista->ini;
-            while(atu != NULL){
-                printf("%d", atu->info);
-                atu = atu->prox;
-            }
-            break;
-            case 2: atu = lista->fim;
-            while(atu != NULL){
-                printf("%d", atu->info);
-                atu = atu->ant;
-            }
-            break;
+            case 1:
+                atu = lista->ini;
+                while(atu != NULL){
+                    printf("%d ", atu->info.posicao);
+                    atu = atu->prox;
+                }
+                break;
+            case 2:
+                atu = lista->fim;
+                while(atu != NULL){
+                    printf("%d ", atu->info.posicao);
+                    atu = atu->ant;
+                }
+                break;
         }
     }
 }
-int remove_listade(tp_listade *lista, tp_item e){
+
+int remove_listade(tp_listade *lista, int posicao_buscada){
     tp_no *atu;
     atu = lista->ini;
-    while((atu != NULL) && (atu->info != e)){
+
+    while((atu != NULL) && (atu->info.posicao != posicao_buscada)){
         atu=atu->prox;
     }
+
     if(atu == NULL) return 0;
+
     if(lista->ini == lista->fim){
         lista->ini = lista->fim = NULL;
     }
@@ -78,7 +90,7 @@ int remove_listade(tp_listade *lista, tp_item e){
             lista->ini = atu->prox;
             atu->prox->ant = NULL;
         }
-        else if(lista->fim == NULL){
+        else if(lista->fim == atu){
             lista->fim = atu->ant;
             atu->ant->prox = NULL;
         }
@@ -88,6 +100,7 @@ int remove_listade(tp_listade *lista, tp_item e){
         }
     }
     free(atu);
+    return 1;
 }
 
 #endif
