f095a172504ca2e6b61b2d094a014061aaa58dbd
dudivis-ide <eduardocostabloisi2007@gmail.com>
2026-05-11 09:24:28 -0300

    pilhade.h adaptado para dinamico

--- pilha.h
+++ pilha.h
@@ -1,76 +1,90 @@
 // Um grande artista assina a sua obra
 
 
-#ifndef pilha_h
-#define pilha_h
+#ifndef PILHADE_H_INCLUDED
+#define PILHADE_H_INCLUDED
+#include <stdlib.h>
 #include <stdio.h>
 #include "tipos.h"
-#define MAX 100
 
+typedef tp_pergunta tp_item_pilha;
 
-typedef tp_pergunta
-	tp_item;
-	
+typedef struct tp_no_pilha_aux {
+  tp_item_pilha info;
+  struct tp_no_pilha_aux *prox;
+} tp_no_pilha;
+
 typedef struct {
-	int topo;
-	tp_item item[MAX];
+  tp_no_pilha *topo;
 } tp_pilha;
 
-void inicializa_pilha( tp_pilha *p){
-	p->topo = -1;
+tp_pilha *inicializa_pilha (){
+   tp_pilha *pilha = (tp_pilha*) malloc(sizeof(tp_pilha));
+   pilha->topo = NULL;
+   return pilha;
 }
 
-int pilha_vazia(tp_pilha *p){
-	if (p->topo == -1) return 1;
-	return 0;
+int pilha_vazia (tp_pilha *pilha){
+   return (pilha->topo == NULL);
 }
 
-int pilha_cheia(tp_pilha *p){
-	if (p->topo == MAX -1) return 1;
-	return 0;
+tp_no_pilha *aloca_pilha() {
+	return (tp_no_pilha*) malloc(sizeof(tp_no_pilha));
 }
 
-int push(tp_pilha *p, tp_item e){
-	if(pilha_cheia(p) == 0){
-		p->topo += 1;
-		strcpy(p->item[p->topo].enunciado, e.enunciado);
-		p->item[p->topo].resposta = e.resposta;
-		return 1;
-	}
-	return 0;
-		
-	
+int push(tp_pilha *pilha, tp_item_pilha e) {
+    tp_no_pilha *novo = aloca_pilha();
+    if (novo == NULL) return 0;
+
+    novo->info = e;
+    novo->prox = pilha->topo;
+    pilha->topo = novo;
+    return 1;
 }
 
-int pop(tp_pilha *p, tp_item *e){
-	if (pilha_vazia(p) == 1){
-		return 0;
-	}
-	strcpy(e->enunciado, p->item[p->topo].enunciado);
-	e->resposta = p->item[p->topo].resposta;
-	p->topo--;
-	return 1;
-}
-int top(tp_pilha *p, tp_item *e){
-		if (pilha_vazia(p) == 1){
-		return 0;
-	}
-	*e = p->item[p->topo];
-	return 1;
+int pop(tp_pilha *pilha, tp_item_pilha *e) {
+    if (pilha_vazia(pilha)) return 0;
+
+    tp_no_pilha *aux = pilha->topo;
+    *e = aux->info;
+
+    pilha->topo = aux->prox;
+    free(aux);
+    return 1;
 }
 
-void imprime_pilha(tp_pilha p){
-	tp_item e;
-	printf("\n");
-	while(!pilha_vazia(&p)){
-		pop(&p, &e);
-		printf("%d\n", e);
-	}
-	printf("\n");
+int top (tp_pilha *pilha, tp_item_pilha *e){
+  if (pilha_vazia(pilha)) return 0;
+  *e = pilha->topo->info;
+  return 1;
 }
 
-int altura_pilha(tp_pilha *p){
-	return p->topo+1;
+tp_pilha *destroi_pilha(tp_pilha *pilha) {
+    tp_item_pilha e;
+    while (!pilha_vazia(pilha)) {
+        pop(pilha, &e);
+    }
+    free(pilha);
+    return NULL;
 }
 
-# endif
+void imprime_pilha(tp_pilha *pilha) {
+     tp_pilha *pilha_aux = inicializa_pilha();
+     tp_item_pilha e;
+
+     printf("Topo -> [ ");
+	 while (!pilha_vazia(pilha)) {
+        pop(pilha, &e);
+        printf("%c ", e.resposta);
+        push(pilha_aux, e);
+     }
+     printf("] <- Fundo\n");
+
+	 while (!pilha_vazia(pilha_aux)) {
+        pop(pilha_aux, &e);
+        push(pilha, e);
+     }
+     destroi_pilha(pilha_aux);
+}
+
+#endif // PILHADE_H_INCLUDED
