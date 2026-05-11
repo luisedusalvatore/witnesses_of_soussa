363e9fabb7a944d23f4fd2c9affe55847263462a
dudivis-ide <eduardocostabloisi2007@gmail.com>
2026-05-11 09:22:10 -0300

    randon.h adaptado para dinamico

--- randon.h
+++ randon.h
@@ -1,14 +1,17 @@
 #ifndef randon_h
 #define randon_h
+#include <stdlib.h>
+
 int rola_dados(){
-    int valor; 
-    valor = (rand()%(6))+1; // Gera um valor aleatorio
+    int valor;
+    valor = (rand()%(6))+1; // gera um valor aleatorio entre 1 e 6
     return valor;
 }
 
-int seletor_de_questão(int qtd){
-    int e; 
-    e = rand() % (qtd); // Gera um valor aleatorio entre 0 e qtd de perguntas
+int seletor_de_questao(int qtd){
+    int e;
+    e = rand() % (qtd); // gera um valor aleatorio entre 0 e qtd-1
     return e;
 }
+
 #endif
