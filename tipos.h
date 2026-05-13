85b4f71b0262b487cf076aee4fd1442f9756e8ae
dudivis-ide <eduardocostabloisi2007@gmail.com>
2026-05-13 17:12:24 -0300

    correção tipos.h

--- tipos.h
+++ tipos.h
@@ -1,60 +1,38 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <locale.h>
-#include <ctype.h>
-#include <time.h>
-#include "tipos.h"
-#include "pilhade.h"
-#include "filade.h"
-#include "randon.h"
-#include "perguntas.h"
-#include "easteregg.h"
-#include "jogadores.h"
-#include "listade.h"
-#include "tabuleiro.h"
 
-int main(){
-    srand(time(NULL)); // gera a seed para os numeros aleatorios
-    int quant;         // quantidade de jogadores
+# ifndef tipos_h
+# define tipos_h
 
-    // declaração da pilha,  fila e lista dinamicas
-    tp_pilha *perguntas;
-    tp_pilha *perguntas_descartadas;
-    tp_fila *jogadores;
-    tp_listade *tabuleiro;
 
-    // inicialização das estruturas dinamicas
-    perguntas = inicializa_pilha();
-    perguntas_descartadas = inicializa_pilha();
-    jogadores = inicializa_fila();
-    tabuleiro = inicializa_listade();
+typedef struct {
+    int cor[4];
+    int tira_carta;
+    int posicao;
+} tp_casa;
 
-    // preenche o tabuleiro com a quantidade de casas que a unidade pede (10,20,30)
-    inicia_tabuleiro(tabuleiro, 20);
+typedef struct tp_no_aux {
+    struct tp_no_aux *ant;
+    tp_casa info;
+    struct tp_no_aux *prox;
+} tp_no;
+typedef struct {
+    tp_no *ini;
+    tp_no *fim;
+} tp_listade;
 
-    // popula e embaralha as cartas
-    popula_perguntas(perguntas);
-    embaralhaQuestoes(perguntas);
+typedef struct{		// struct de jogadores
 
-    setlocale(LC_ALL, "Portuguese"); // função responsável por adicionar caracteres do PT-BR
+    char nome[25];
+    int cor;
+    tp_no *posicao;
 
-    quant = quantidade(); // atribuição da quantidade de jogadores
+}Player;
 
-    // função para colocar os jogadores no tabuleiro
-    lerDados(quant, jogadores, tabuleiro);
+typedef struct{
 
-    rodadaplayer(jogadores, perguntas, perguntas_descartadas); // realiza uma rodada
+    char enunciado[300];
+    char resposta;
+} tp_pergunta;
 
-    // limpar memória
-    destroi_pilha(perguntas);
-    destroi_pilha(perguntas_descartadas);
-    destroi_fila(jogadores);
-    while (!listade_vazia(tabuleiro)) {
-        remove_listade(tabuleiro, tabuleiro->ini->info.posicao);
-    }
-    free(tabuleiro);
-
-    return 0;
-}
-
+# endif
