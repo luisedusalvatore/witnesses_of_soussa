0e5e4774fe2fcc5ea118672661666e2bbd146f95
CarlosMonteiroTweaks <carlosmonteironeto2006@gmail.com>
2026-05-17 10:55:22 -0300

    adionei funções que veficam se a posição do jogador é um primo, palindromo em binário ou um número da sequencia de fibonacci (ta abstrato a parte de binário).

--- tabuleiro.h
+++ tabuleiro.h
@@ -3,20 +3,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "listade.h"
+#include "pilhade.h"
 
 int eh_primo(int posi){
-    // implementar lógica de número primo real
+    // implementar lógica que verifica se o número da casa é primo
+    for (int i = 2; i < posi; i++){
+        if (posi % i != 0) return 0;
+    }
     return 1;
 }
+
 
 int eh_binario(int posi){
-    // implementar lógica de potência de 2 real
-    return 1;
+    // implementar lógica que verifica se o número da casa é um palindromo em binário
+    char vetor[30]; 
+    int tam = 0;
+    tp_pilha *pilha = inicializa_pilha();
+	tp_item_pilha e;
+    // transforma o número em binário e armazena no vetor
+    if (posi == 0) {
+        vetor[tam++] = '0';
+    } else {
+        while (posi > 0) {
+            
+            vetor[tam++] = (posi % 2) + '0'; 
+            posi = posi / 2;            
+        }
+    }
+
+    // armazena os elementos na pilha
+    for (int i = 0; i < tam; i++) {
+        tp_item_pilha n;
+        n.resposta = vetor[i];
+        push(pilha, n);
+    }
+
+   // compara com o vetor
+    int eh_palindromo = 1; 
+
+    for (int i = 0; i < tam; i++) {
+        
+        pop(pilha, &e); 
+
+        if (vetor[i] != e.resposta) {
+            eh_palindromo = 0; // Não é um palíndromo
+            break;             
+        }
+    }
+
+    destroi_pilha(pilha);
+
+    return eh_palindromo;
 }
 
+
 int eh_fibo(int posi){
-    // implementar lógica da sequência de Fibonacci real
-    return 1;
+    // implementar lógica que verifica se a casa tem número que faz parte da sequência de Fibonacci
+    if (posi == 1) return 1;
+    
+    int a = 0, b = 1, c = a + b; 
+
+    while (c < posi) {
+        a = b;    
+        b = c;    
+        c = a + b; 
+    }
+
+    //fazendo a comparação
+    if (c == posi) return 1;  
+        
+    return 0; 
+    
 }
 
 int tem_carta(int posi){ // Define se a casa vai ter carta
