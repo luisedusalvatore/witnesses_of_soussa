d91a268dbfc316fee11f72f47eec240c7fef76ab
dudivis-ide <eduardocostabloisi2007@gmail.com>
2026-05-17 18:56:10 -0300

    gacha de itens implementado em randon.h

--- randon.h
+++ randon.h
@@ -1,11 +1,12 @@
 #ifndef randon_h
 #define randon_h
 #include <stdlib.h>
+#include <stdio.h>
 
 int rola_dados(){
     int valor;
-    valor = (rand()%(6))+1; // gera um valor aleatorio entre 1 e 6
-    printf("Numero rolado: %d", valor)
+    valor = (rand()%(6))+1;
+    printf("Numero rolado: %d\n", valor);
     return valor;
 }
 
@@ -13,6 +14,25 @@ int seletor_de_questao(int qtd){
     int e;
     e = rand() % (qtd); // gera um valor aleatorio entre 0 e qtd-1
     return e;
+}
+
+int sortear_item() {
+    int chance = rand() % 100; // gera de 0 a 99
+
+    // 60% de chance de vir um raro (IDs 1, 2, 7)
+    if (chance < 60) {
+        int itens_raros[] = {1, 2, 7};
+        return itens_raros[rand() % 3];
+    }
+    // 30% de chance de vir um mítico (IDs 3, 4, 6)
+    else if (chance < 90) {
+        int itens_miticos[] = {3, 4, 6};
+        return itens_miticos[rand() % 3];
+    }
+    // 10% de chance de vir o lendário (ID 5 - Buraco de Minhoca)
+    else {
+        return 5;
+    }
 }
 
 #endif
