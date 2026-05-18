0c7675abcae79dec7944d5e28f8a991a194e42a7
CarlosMonteiroTweaks <carlosmonteironeto2006@gmail.com>
2026-05-18 18:18:53 -0300

    botei as funções corretas

--- tabuleiro.h
+++ tabuleiro.h
@@ -4,30 +4,56 @@
 #include <stdlib.h>
 #include "listade.h"
 
+
 int eh_primo(int posi){
-    if (posi <= 1) return 0;
-    for (int i = 2; i * i <= posi; i++) {
-        if (posi % i == 0) return 0;
+    // implementar lógica que verifica se o número da casa é primo
+    for (int i = 2; i < posi; i++){
+        if (posi % i != 0) return 0;
     }
     return 1;
 }
 
+
 int eh_binario(int posi){
-    if (posi <= 0) return 0;
-    return (posi & (posi - 1)) == 0;
-}
-
-int eh_fibo(int posi){
-    if (posi == 0 || posi == 1) return 1;
-
-    int a = 0, b = 1, c = a + b;
-    while (c <= posi) {
-        if (c == posi) return 1;
-        a = b;
-        b = c;
-        c = a + b;
+    // implementar lógica que verifica se o número da casa é um palindromo em binário
+    char vetor[30]; 
+    int tam = 0;
+    tp_pilha *pilha = inicializa_pilha();
+	tp_item_pilha e;
+    // transforma o número em binário e armazena no vetor
+    if (posi == 0) {
+        vetor[tam++] = '0';
+    } else {
+        while (posi > 0) {
+            
+            vetor[tam++] = (posi % 2) + '0'; 
+            posi = posi / 2;            
+        }
     }
-    return 0;
+
+    // armazena os elementos na pilha
+    for (int i = 0; i < tam; i++) {
+        tp_item_pilha n;
+        n.resposta = vetor[i];
+        push(pilha, n);
+    }
+
+   // compara com o vetor
+    int eh_palindromo = 1; 
+
+    for (int i = 0; i < tam; i++) {
+        
+        pop(pilha, &e); 
+
+        if (vetor[i] != e.resposta) {
+            eh_palindromo = 0; // Não é um palíndromo
+            break;             
+        }
+    }
+
+    destroi_pilha(pilha);
+
+    return eh_palindromo;
 }
 
 // retorna 0 (normal), 1 (pergunta) ou 2 (baú)
