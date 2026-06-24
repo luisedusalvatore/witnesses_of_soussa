b4f74015948dc58020d25bf7d528775c4a2f4c80
dudivis-ide <eduardocostabloisi2007@gmail.com>
2026-06-24 03:45:49 -0300

    Update hank.h

--- hank.h
+++ hank.h
@@ -7,41 +7,41 @@
 #include "arvavlr.h"
 #include "tipos.h"
 
-// Mantemos isso para inicializar o jogador
+// inicializar o jogador
 int inicializa_hank(Player *jogador){
     jogador->dados.acertos = 0;
     jogador->dados.erros = 0;
     jogador->dados.score = 0;
 
-    // CORREÇÃO: Copia o nome do jogador para a estrutura de dados que será salva no txt!
+    // copia o nome do jogador para a estrutura de dados que sera salva no txt
     strcpy(jogador->dados.nome, jogador->nome);
 
     return 1;
 }
 
-// Função recursiva para percorrer a árvore AVL (In-Ordem) e imprimir no arquivo
+// função recursiva para percorrer a árvore AVL e imprimir no arquivo
 void grava_no_arquivo(ArvAVL* raiz, FILE *arquivo){
     if(raiz == NULL || *raiz == NULL) return;
 
-    grava_no_arquivo(&((*raiz)->esq), arquivo); // Vai para a esquerda
+    grava_no_arquivo(&((*raiz)->esq), arquivo);
 
     fprintf(arquivo, "Casa: %02d | Visitas: %02d | Acertos: %02d | Erros: %02d\n",
             (*raiz)->info.numero_casa, (*raiz)->info.vezes_caiu,
             (*raiz)->info.acertos, (*raiz)->info.erros);
 
-    grava_no_arquivo(&((*raiz)->dir), arquivo); // Vai para a direita
+    grava_no_arquivo(&((*raiz)->dir), arquivo);
 }
 
-// Gera o arquivo de relatório final exigido pelo professor
+// gera o arquivo de relatório final
 void salva_relatorio_casas(ArvAVL* arvore){
-    // Usamos "w" (Write) para criar um relatório fresco a cada partida jogada
+
     FILE *arquivo = fopen("relatorio_casas.txt", "w");
 
     if(arquivo != NULL) {
         fprintf(arquivo, "--- RELATORIO DE ESTATISTICAS DO TABULEIRO ---\n");
         fprintf(arquivo, "(Gerado atraves de percurso In-Ordem na Arvore AVL)\n\n");
 
-        grava_no_arquivo(arvore, arquivo); // Manda a árvore se imprimir no arquivo
+        grava_no_arquivo(arvore, arquivo); // manda a árvore se imprimir no arquivo
 
         fclose(arquivo);
     }
